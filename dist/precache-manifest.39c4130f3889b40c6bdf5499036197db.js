379585266c32414343c9a4a7a7256bd7ac67e9a9
Nicolò Maria Semprini <nicosemp@gmail.com>
2021-04-13 11:20:09 +0200

    remove /dist from .gitignore + add vue-config.js

---
+++ dist/precache-manifest.39c4130f3889b40c6bdf5499036197db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d94f26e070ca713dcc9",
+    "url": "/anyversary/css/app.77796b4e.css"
+  },
+  {
+    "revision": "6c86583a83866c67e979c3ee8878d749",
+    "url": "/anyversary/img/logo.6c86583a.png"
+  },
+  {
+    "revision": "98e1b73c48903ef651ea49a94d3272d8",
+    "url": "/anyversary/index.html"
+  },
+  {
+    "revision": "02d163df13d0eafa67ca",
+    "url": "/anyversary/js/about.f1ec0cbb.js"
+  },
+  {
+    "revision": "0d94f26e070ca713dcc9",
+    "url": "/anyversary/js/app.dbab02cd.js"
+  },
+  {
+    "revision": "a26a619535d0f9db3207",
+    "url": "/anyversary/js/chunk-vendors.3544e6d9.js"
+  },
+  {
+    "revision": "0711141873d2fd3d7f67b2bb6a19f7be",
+    "url": "/anyversary/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/anyversary/robots.txt"
+  }
+]);
