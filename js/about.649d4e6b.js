cb6133b23ed3c87ffdd82518b7ad50af9ec45a54
Nicolò Maria Semprini <nicosemp@gmail.com>
2021-08-10 14:32:25 +0200

    deploy

---
+++ js/about.649d4e6b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),u={class:"about"},c=Object(e["h"])("h1",null,"This is an about page",-1),o=[c];function s(n,t){return Object(e["u"])(),Object(e["g"])("div",u,o)}const b={};b.render=s;t["default"]=b}}]);
+//# sourceMappingURL=about.649d4e6b.js.map
---
+++ precache-manifest.2d8d0e228e4fa752ec5b5ded1d5a1983.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adaa987ddd1dc0430da",
+    "url": "/AnyVersary/css/app.c82cc4c2.css"
+  },
+  {
+    "revision": "6c86583a83866c67e979c3ee8878d749",
+    "url": "/AnyVersary/img/logo.6c86583a.png"
+  },
+  {
+    "revision": "28b85ffb0b4e060b6609fc7a95b770fd",
+    "url": "/AnyVersary/index.html"
+  },
+  {
+    "revision": "c4fb23eccb28be937fca",
+    "url": "/AnyVersary/js/about.649d4e6b.js"
+  },
+  {
+    "revision": "3adaa987ddd1dc0430da",
+    "url": "/AnyVersary/js/app.7f4b5caf.js"
+  },
+  {
+    "revision": "ab0cbc576930101bf2bf",
+    "url": "/AnyVersary/js/chunk-vendors.17cc2258.js"
+  },
+  {
+    "revision": "0711141873d2fd3d7f67b2bb6a19f7be",
+    "url": "/AnyVersary/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/AnyVersary/robots.txt"
+  }
+]);
